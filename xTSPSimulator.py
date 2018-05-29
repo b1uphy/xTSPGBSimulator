9406882291d82f5d10c4c9a60cdc280f9c5745c9
bluphy <bluphyxw@gmail.com>
2018-05-29 18:00:55 +0800

    Extended the structure.  Please enter the commit message for your changes. Lines starting

--- xTSPSimulator.py
+++ xTSPSimulator.py
@@ -8,26 +8,48 @@ import asyncio
 import functools
 from  async_timeout import timeout
 
-TIMER_OTA_MSG_TIMEOUT = 3
+TIMER_OTA_MSG_TIMEOUT = 30
 
-CRLF = b'\r\n'
-PROMPT = b'?> '
+# BEGIN APP Layer
+def writedb(VIN:str,systime,msgtime,msg:bytes,db):
+    pass
 
-async def handle_queries(reader, writer):  # <3>
+def 
+
+# END APP layer
+
+# BEGIN OTA Layer
+def dispatch(msg:bytes)->int:
+    return 0
+
+
+# END OTA layer
+
+
+async def handle_vehicle_connection(reader, writer):  # <3>
     client = writer.get_extra_info('peername')
     print('Client {} connected.'.format(client))
     counter = 0
     while True:  # <4>
+        counter += 1
+        print('counter=',counter)
+        print(client,' Waiting msg...')
         try:
-            header = await reader.readexactly(24)  # <7>
-        except :
+            async with timeout(TIMER_OTA_MSG_TIMEOUT):
+                header = await reader.readexactly(24)  # <7>
+        except asyncio.TimeoutError:
+            print('Rx timeout')
+            print('Close connection because of timeout')
+            break
+        except:
+            print('Connection broken!')
             break
 
         if header:
-            print('Received header {0}:\t{1}'.format(client,header))
+            print('Received header {0}:\t{1}'.format(client,header.hex()))
             lengthraw = header[-2:]
             print('lengthraw:{}'.format(lengthraw))
-            length = int.from_bytes(lengthraw, byteorder='big')+1
+            length = int.from_bytes(lengthraw, byteorder='big')+1 # the length including the sum byte
             print('length:{}'.format(length))
 
             data = None
@@ -37,15 +59,18 @@ async def handle_queries(reader, writer):  # <3>
             except asyncio.TimeoutError:
                 print('Rx timeout')
                 print('Close connection because of timeout')
+                break
             
 
-            counter += 1
-            print('counter=',counter)
+            
             
             if data:
-                print('Received from {0}:\t{1}'.format(client,data))  # <10>
+                msg = header+data
+                print('Received from {0}:\t{1}'.format(client,msg.hex()))  # <10>
+                
             
         else:
+            print('Nothing received')
             break
     print('Close the client socket')  # <17>
     writer.close()  # <18>
@@ -55,11 +80,12 @@ async def handle_queries(reader, writer):  # <3>
 def main(address='127.0.0.1', port=9201):  # <1>
     port = int(port)
     loop = asyncio.get_event_loop()
-    server_coro = asyncio.start_server(handle_queries, address, port, loop=loop) # <2>
+    server_coro = asyncio.start_server(handle_vehicle_connection, address, port, loop=loop) # <2>
     server = loop.run_until_complete(server_coro) # <3>
 
     host = server.sockets[0].getsockname()  # <4>
     print('Serving on {}. Hit CTRL-C to stop.'.format(host))  # <5>
+
     try:
         loop.run_forever()  # <6>
         print('Can not be here')
