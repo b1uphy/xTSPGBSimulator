53de166d7940c756db7507a8d4cbeab50435c096
bluphy <bluphyxw@gmail.com>
2018-05-30 18:49:35 +0800

    Working, not finished.

---
+++ xOTAGB.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# bluphy@163.com
+# 2018-5-29 17:06:51 by xw: new created.
+CMD = {
+    b'\x01' : '车辆登入',    
+    b'\x02' : '实时数据',    
+    b'\x03' : '补发数据',    
+    b'\x04' : '车辆登出',    
+    b'\x07' : '心跳',    
+    b'\x80' : '查询命令',    
+    b'\x81' : '设置命令',   
+    b'\x82' : '控制命令'
+    }
+class Field:
+    def __init__(self,name,raw,convertfunc=None,checkfunc=None):
+        print(raw)
+        self.name = name
+
+        if type(raw) is int:
+            self.raw = raw.to_bytes(1,byteorder='big')
+        else:
+            self.raw = raw
+        print(type(self.raw))
+        if convertfunc:          
+            self.phy = convertfunc(self.raw)
+        else:
+            self.phy = self.raw.hex()
+
+class Head:
+    def __init__(self, header:bytes):
+        print(header[2])
+        self.cmd = Field('命令标识', header[2], convertfunc=lambda x: CMD[x])
+        self.resflg = Field('应答标志', header[3])
+        self.VIN = Field('VIN', header[4:21],convertfunc=lambda x: x.decode('ascii'))
+        self.secretflg = Field('加密方式', header[21])
+        self.length = Field('数据长度', header[22:24], convertfunc=lambda x: int(x.hex(),16))
+
+class GBTime:
+    def __init__(self,buf:bytes):
+        pass
+class OTAGBData:
+    def __init__(self, msg:bytes):
+        '''
+        接收字节流数据作为初始化OTAGB消息的参数，对消息进行初始化
+        '''
+        if self.detectMsgFormat(msg):
+            self.head = Head(msg[:24])
+            self.payload = msg[24:-1]
+            self.chk = msg[-1]
+
+    def detectMsgFormat(self, msg:bytes)->bool:
+        '''
+        检测数据是否符合规范，True符合规范，False不符合规范
+        '''
+        pass
+        return True
+
+def calBCCChk(buf:bytes):
+    chk = buf[0]
+    for byte in buf[1:]:
+        chk = byte^chk
+    return chk
+
+def createOTAGBMsg(cmd:str, resp:str, VIN:str, secrettype:int, length:int, data:bytes):
+    pass
+
+
+if __name__ == '__main__':
+    msg = b'##\x04\xFELXVJ2GFC2GA030003\x01\x00\x08\x11\x11\x11\x11\x11\x11\x33\x33\x33'
+    gbdata = OTAGBData(msg)
+    print(gbdata.head.cmd.phy)
+    msg2 = '232302FE4C4D47464531473030303030303053593101013512051005223101020301FFFF00000000000007D00002000000FF0002010103000FA043F800000013880501000000000000000006010100000101000001010001010007000000000000000000080101000007D0006000016000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009010100180000000000000000000000000000000000000000000000008F'
+    msg3 = '232301FE4C4D47464531473030303030303053593101001E1205100B0B30000138393836303631363031303035343538373630310100EC'
+    tmp = bytes.fromhex(msg2)[2:-1]
+    print(tmp)
+    chk = calBCCChk(tmp)
+  
+    print('chk=',chk.to_bytes(1,byteorder='big').hex())
--- xTSPSimulator.py
+++ xTSPSimulator.py
@@ -7,22 +7,80 @@ import sys
 import asyncio
 import functools
 from  async_timeout import timeout
-
+from xOTAGB import OTAGBData,createOTAGBMsg
 TIMER_OTA_MSG_TIMEOUT = 30
 
+gInterrupt_flagstr = ''
 # BEGIN APP Layer
-def writedb(VIN:str,systime,msgtime,msg:bytes,db):
-    pass
+class Vehicle:
+    def __init__(self,VIN:str, reader, writer,):
+        self.VIN = VIN
+        self.state = 'connected'
+        self.data = None
+        self.msg = None
 
-def 
+    def processMsg(self,msg):
+        self.data = OTAGBData(msg)
+        if self.data.head.cmd.phy == '车辆登入':
+            self.state = 'Login'
+        elif self.data.head.cmd.phy == '实时数据' or self.data.head.cmd.phy == '补发数据':
+            pass
+        elif self.data.head.cmd.phy == '车辆登出':
+            self.state = 'Logout'
+        elif self.data.head.cmd.phy == '心跳':
+            pass
+        else:
+            print('Error CMD')
+
+    def writedb(self, VIN:str, systime, msgtime, direction,msg:bytes, db):
+        pass
+
 
 # END APP layer
 
 # BEGIN OTA Layer
-def dispatch(msg:bytes)->int:
-    return 0
+async def receiveMsg(client,reader)->bytes:
+    result = {'msg':None, 'responsecode':0}
+    header = None
+    try:
+        async with timeout(TIMER_OTA_MSG_TIMEOUT):
+            header = await reader.readexactly(24) 
+    except asyncio.TimeoutError:
+        print('Rx timeout')
+        print('Close connection because of timeout')
+        result['responsecode'] = 'Timeout!'
+    except:
+        print('Connection broken!')
+        result['responsecode'] = 'Connection broken'
 
+    if header:
+        print('Received header {0}:\t{1}'.format(client,header.hex()))
+        lengthraw = header[-2:]
+        print('lengthraw:{}'.format(lengthraw))
+        length = int.from_bytes(lengthraw, byteorder='big')+1 # the length including the sum byte
+        print('length:{}'.format(length))
 
+        data = None
+        try:
+            async with timeout(TIMER_OTA_MSG_TIMEOUT):
+                data = await reader.readexactly(length)
+        except asyncio.TimeoutError:
+            print('Rx timeout')
+            print('Close connection because of timeout')
+            result['responsecode'] = 'Timeout!'
+        
+        if data:
+            result['msg'] = header+data
+            print('Received from {0}:\t{1}'.format(client,result['msg'].hex()))  # <10>
+
+    return result
+
+async def sendMsg(writer,msg:bytes):
+    try:
+        writer.write(msg)
+    except:
+        print('Send Msg fail. Msg:',msg.hex())
+    
 # END OTA layer
 
 
@@ -60,9 +118,6 @@ async def handle_vehicle_connection(reader, writer):  # <3>
                 print('Rx timeout')
                 print('Close connection because of timeout')
                 break
-            
-
-            
             
             if data:
                 msg = header+data
