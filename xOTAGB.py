8f921ce295f5186651de382d1afa882c74246fac
b1uphy <bluphy@163.com>
2018-05-30 23:33:58 +0800

    Implement login/logout response.

--- xOTAGB.py
+++ xOTAGB.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # bluphy@163.com
 # 2018-5-29 17:06:51 by xw: new created.
-CMD = {
+import time
+from bidict import bidict
+CMD = bidict({
     b'\x01' : '车辆登入',    
     b'\x02' : '实时数据',    
     b'\x03' : '补发数据',    
@@ -10,7 +12,7 @@ CMD = {
     b'\x80' : '查询命令',    
     b'\x81' : '设置命令',   
     b'\x82' : '控制命令'
-    }
+    })
 class Field:
     def __init__(self,name,raw,convertfunc=None,checkfunc=None):
         print(raw)
@@ -59,10 +61,20 @@ def calBCCChk(buf:bytes):
     chk = buf[0]
     for byte in buf[1:]:
         chk = byte^chk
-    return chk
+    return chk.to_bytes(1,byteorder='big')
 
-def createOTAGBMsg(cmd:str, resp:str, VIN:str, secrettype:int, length:int, data:bytes):
-    pass
+def genGBTime()->bytes:
+    '''
+    生成国标格式的时间字节流
+    '''
+    return b'123456'
+
+def createOTAGBMsg(cmd:bytes, resp:bytes, VIN:bytes, secrettype:int, length:int, data:bytes):
+    start = b'##'
+    payload = cmd+resp+VIN+secrettype.to_bytes(1,byteorder='big')+length.to_bytes(2,byteorder='big')+data
+    chk = calBCCChk(payload)
+    msg = start + payload + chk
+    return msg
 
 
 if __name__ == '__main__':
@@ -75,4 +87,6 @@ if __name__ == '__main__':
     print(tmp)
     chk = calBCCChk(tmp)
   
-    print('chk=',chk.to_bytes(1,byteorder='big').hex())
+    print('chk=',chk.hex())
+
+    print(createOTAGBMsg(b'\x01', b'\xFE', 'LXVJ2GFC2GA030003', 1, 7, genGBTime() ))
--- xTSPSimulator.py
+++ xTSPSimulator.py
@@ -7,30 +7,58 @@ import sys
 import asyncio
 import functools
 from  async_timeout import timeout
-from xOTAGB import OTAGBData,createOTAGBMsg
+from xOTAGB import OTAGBData,createOTAGBMsg,CMD,genGBTime
 TIMER_OTA_MSG_TIMEOUT = 30
 
 gInterrupt_flagstr = ''
 # BEGIN APP Layer
 class Vehicle:
-    def __init__(self,VIN:str, reader, writer,):
-        self.VIN = VIN
+    def __init__(self,client):
+        self.client = client
+        self.VIN = None
         self.state = 'connected'
         self.data = None
         self.msg = None
 
-    def processMsg(self,msg):
+    def processMsg(self,msg)->bytes:
+        result = {'msg':None, 'responsecode':0}
         self.data = OTAGBData(msg)
+        if self.VIN:
+            if self.VIN != self.data.head.VIN.raw:
+                self.VIN = self.data.head.VIN.raw
+                print('VIN is not match')
+        else:
+            self.VIN = self.data.head.VIN.raw
+
         if self.data.head.cmd.phy == '车辆登入':
             self.state = 'Login'
+            print('Vehicle login: VIN = ',self.VIN.decode('ascii'))
+            result = self.responseLogin()
+
         elif self.data.head.cmd.phy == '实时数据' or self.data.head.cmd.phy == '补发数据':
             pass
+
         elif self.data.head.cmd.phy == '车辆登出':
-            self.state = 'Logout'
+            print('Vehicle logout: VIN = ',self.VIN.decode('ascii'))
+            result = self.responseLogout()
+
         elif self.data.head.cmd.phy == '心跳':
-            pass
+            result = self.responseHeartbeat()
         else:
             print('Error CMD')
+            result['responsecode'] = 'Error CMD'
+        
+        return result
+
+    def responseLogin(self):
+        result = {'msg':None, 'responsecode':0}
+        result['msg'] = createOTAGBMsg(CMD.inv['车辆登入'], b'\xFE', self.VIN, 1, 7, genGBTime())
+        print("response result['msg']",result['msg'])
+        return result
+
+    def responseLogout(self):
+        result = {'msg':None, 'responsecode':'Close Connection'}
+        return result
 
     def writedb(self, VIN:str, systime, msgtime, direction,msg:bytes, db):
         pass
@@ -80,7 +108,8 @@ async def sendMsg(writer,msg:bytes):
         writer.write(msg)
     except:
         print('Send Msg fail. Msg:',msg.hex())
-    
+    else:
+        print('Send Msg:',msg.hex())
 # END OTA layer
 
 
@@ -88,45 +117,24 @@ async def handle_vehicle_connection(reader, writer):  # <3>
     client = writer.get_extra_info('peername')
     print('Client {} connected.'.format(client))
     counter = 0
+    vhl = Vehicle(client)
     while True:  # <4>
         counter += 1
         print('counter=',counter)
         print(client,' Waiting msg...')
-        try:
-            async with timeout(TIMER_OTA_MSG_TIMEOUT):
-                header = await reader.readexactly(24)  # <7>
-        except asyncio.TimeoutError:
-            print('Rx timeout')
-            print('Close connection because of timeout')
-            break
-        except:
-            print('Connection broken!')
-            break
-
-        if header:
-            print('Received header {0}:\t{1}'.format(client,header.hex()))
-            lengthraw = header[-2:]
-            print('lengthraw:{}'.format(lengthraw))
-            length = int.from_bytes(lengthraw, byteorder='big')+1 # the length including the sum byte
-            print('length:{}'.format(length))
-
-            data = None
-            try:
-                async with timeout(TIMER_OTA_MSG_TIMEOUT):
-                    data = await reader.readexactly(length)
-            except asyncio.TimeoutError:
-                print('Rx timeout')
-                print('Close connection because of timeout')
-                break
-            
-            if data:
-                msg = header+data
-                print('Received from {0}:\t{1}'.format(client,msg.hex()))  # <10>
-                
-            
+        result = await receiveMsg(client,reader)
+        if result['responsecode'] == 0:
+            msg = result['msg']
         else:
-            print('Nothing received')
             break
+        
+        result = vhl.processMsg(msg)
+        if result['responsecode'] == 'Close Connection':
+            break
+        responseMsg =result['msg']
+        if responseMsg:
+            await sendMsg(writer,responseMsg)
+
     print('Close the client socket')  # <17>
     writer.close()  # <18>
 # END xTSPSimulator_TOP
